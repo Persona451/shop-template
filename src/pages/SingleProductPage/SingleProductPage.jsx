fc6e0c5afdb6e14dabd66e38329f70f441d7e214
Persona451 <sultankairat1528@gmail.com>
2023-05-14 00:18:09 +0600

    cartpage

--- src/pages/SingleProductPage/SingleProductPage.jsx
+++ src/pages/SingleProductPage/SingleProductPage.jsx
@@ -1,54 +1,12 @@
-import React, { useState, useEffect } from "react";
-import styles from './singleproduct.module.css'
-import { useParams } from "react-router-dom";
-import servicesApi from "../../services/product";
-import { addToCart } from "../../redux/cartSlice";
-import { useDispatch } from "react-redux";
+import React from 'react';
+import SingleProductCard from '../../components/SingleProductCard/SingleProductCard';
 
-const SingleProductCard = (props) => {
-    const [product, setProduct] = useState({});
-    const [quantity, setQuantity] = useState(1);
-    const { id } = useParams();
-    const dispatch = useDispatch();
-    
-    useEffect(() => {
-        document.documentElement.scroll({
-            top: "0",
-            behavior: "smooth"
-        });
-        servicesApi.getProduct(id).then((res) => {
-          setProduct(res.data);
-        });
-    }, []);
-
-    const date = new Date(product.createdAt);
-    const options = { day: "numeric", month: "short", year: "numeric" };
-    const formattedDate = date.toLocaleDateString("en-US", options);
-    
+const SingleProductPage = () => {
     return (
-        <div className={styles.card}>
-            <div className={styles.wrapper}>
-                <div className={styles["img-wrapper"]}>
-                    <img src={product.img} alt="" />
-                </div>
-                <div className={styles.info}>
-                    <h1 className={styles.title}>{product.title}</h1>
-                    <p className={styles.price}>{product.price}</p>
-                    <p className={styles.descr}>{product.descr}</p>
-                    <div className={styles["control-wrapper"]}>
-                        <div className={styles.quantity}>
-                            <button className={styles["quantity-control"]} onClick={() => quantity > 1 && setQuantity(quantity - 1)}>-</button> 
-                                <span>{quantity}</span>
-                            <button className={styles["quantity-control"]} onClick={() => setQuantity(quantity + 1)}>+</button>
-                        </div>
-                        <button className={styles.add} onClick={() => dispatch(addToCart({...product, quantity}))} >
-                            Add To Cart
-                        </button>
-                    </div>
-                </div>
-            </div>
+        <div>
+            <SingleProductCard/>
         </div>
     );
 };
 
-export default SingleProductCard;
+export default SingleProductPage;
