94fa58d513a7296eaee08d2e4d78ef8144d7069c
Persona451 <sultankairat1528@gmail.com>
2023-05-13 23:15:00 +0600

    total

--- src/components/CheckoutForm/CheckoutForm.jsx
+++ src/components/CheckoutForm/CheckoutForm.jsx
@@ -6,26 +6,30 @@ import { CardElement } from '@stripe/react-stripe-js';
 
 const CheckoutForm = () => {
   const sum = useSelector(state => state.cart.sum);
-    return (
-      <>
+  const products = useSelector(state => state.cart.productsCart);
+  return (
+    <>
       <Breadcrumbs />
       <h1>Сумма оплаты: {sum}</h1>
       <div className={styles.wrapper}>
-      <form className={styles.card}>
-        <div>
-          <CardElement />
+        <form className={styles.card}>
+          <div>
+            <CardElement />
+          </div>
+          <button>Pay</button>
+        </form>
+        <div className={styles["product-wrapper"]}>
+          <p className={styles["product-title"]}> Product  <span>Subtotal</span> </p>
+          {products.map((product) => (
+            <div>
+              <p className={styles["product-name"]}>{product.title} - {product.price} X {product.quantity}<span>{product.price * product.quantity}</span> </p>
+            </div>
+          ))}
+          <p className={styles["product-total"]}> Total <span>{sum}</span> </p>
         </div>
-        <button>Pay</button>
-      </form>
-      <div className={styles["product-wrapper"]}>
-        <p className={styles["product-title"]}> Product  <span>Subtotal</span> </p>
-        <p className={styles["product-name"]}>  Asgaard sofa  <span>Rs. 250000</span> </p>
-        <p className={styles["product-subtotal"]}>  Asgaard sofa  <span>Rs. 250000</span> </p>
-        <p className={styles["product-total"]}> Total  <span>Rs. 250000</span> </p>
       </div>
-      </div>
-      </>
-    );
+    </>
+  );
 };
 
 export default CheckoutForm;
